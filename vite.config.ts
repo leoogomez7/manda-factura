e688dd7b87a5aebf292e7ab0d8c210ab4f036a8e
leoogomez7 <campeonleonardo@hotmail.com>
2026-06-05 11:22:56 -0300

    correcion subida web

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    ...tanstackStart(), // <-- Dejalo activo sin la opción "spa", para que maneje el SSR
-    react(),
     tsconfigPaths(),
     tailwindcss(),
+    react(),
   ],
   resolve: {
     alias: [
