dae84d9925f6f162e99edde816ec646ade694cc4
leoogomez7 <campeonleonardo@hotmail.com>
2026-06-05 14:39:25 -0300

    fix build for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,20 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   plugins: [
-    ...tanstackStart({
-      spa: {
-        enabled: true
-      }
-    }),
     react(),
     tsconfigPaths(),
     tailwindcss(),
+    tanstackRouter(),
   ],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: new URL("./src", import.meta.url).pathname,
-      },
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
